2bac0abffbfa255feafd9ea4013fbf5cda0d5201
Pratyasha Priyadarshini <priyadarshinipratyasha7@gmail.com>
2025-07-06 09:18:18 +0530

    Adding loops

---
+++ Loops/Hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hello{
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter <= 100){
+            System.out.println("Hello");
+            counter ++;
+        }
+        System.out.println("Printing HW 100x");
+    }
+   
+}
